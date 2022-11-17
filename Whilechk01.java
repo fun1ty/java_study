14c97906d1ed1c31404aed43cb1e519ffda10a31
kmr <ettedlop@gmail.com>
2022-11-17 21:11:09 +0900

    forstar

---
+++ Whilechk01.java
@@ -0,0 +1,20 @@
+package classes;
+class  Whilechk01
+{
+	public static void main(String[] args) 
+	{ int idx=0;
+		for (;idx<3;)
+		{
+			idx++;
+			System.out.println("niceday");
+		}
+		System.out.println("--------------------");
+		
+		idx=0;
+		while (idx<3)
+		{
+			idx++;
+			System.out.println("niceday");
+		}
+	}
+}
