21224ce8829b6f1f8d734c9529b055ba5e5b067b
kmr <ettedlop@gmail.com>
2022-11-20 14:50:12 +0900

    string

---
+++ StringAPI01_01.java
@@ -0,0 +1,11 @@
+class StringAPI01_01
+{
+	public static void main(String[] args) 
+	{
+		String str="HappyDay";
+		System.out.println(str.charAt(5));
+		System.out.println(str.indexOf('D'));
+		System.out.println(str.indexOf(68));
+		System.out.println(str.indexOf("Day"));
+	}
+}
---
+++ StringAPI02.java
@@ -0,0 +1,13 @@
+class StringAPI02 
+{
+	public static void main(String[] args) 
+	{
+		String str="HappyDay";
+		System.out.println(str.compareTo("happyDay"));
+		System.out.println(str.compareTo("HappyAay"));
+
+		System.out.println(str.compareToIgnoreCase("happyDay"));
+
+
+	}
+}
