e330e6a83f74d57d77b9010dab82ff4fdbab1efd
fun1ty <116661333+fun1ty@users.noreply.github.com>
2022-11-24 18:51:41 +0900

    Ex
    Ex

---
+++ PrintEx01.java
@@ -0,0 +1,14 @@
+public class PrintEx01{
+  public static void main(String[] args){
+      java.lang.System.out.print("HAVE");
+      java.lang.System.out.print("A");
+      java.lang.System.out.print("Nice Day");
+
+      java.lang.System.out.println();
+
+      java.lang.System.out.println("HAVE");
+      java.lang.System.out.println("A");
+      java.lang.System.out.println("Nice Day");
+  
+    }
+}
