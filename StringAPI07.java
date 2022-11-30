b25a4805fd30275ebdf55e05f97c13e3c80cd388
kmr <ettedlop@gmail.com>
2022-11-30 21:00:16 +0900

    string

---
+++ StringAPI07.java
@@ -0,0 +1,10 @@
+class  StringAPI07
+{
+	public static void main(String[] args) 
+	{ String str01=" Nice "; String str02=" nice ";
+		System.out.println("Have A"+str01+"Day");
+		System.out.println("Have A"+str01.trim()+"Day");
+		System.out.println(str01.equals(str02));
+		System.out.println(str01.equalsIgnoreCase(str02));
+	}
+}
