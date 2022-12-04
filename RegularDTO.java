8b07e1752b9106941cf6401421d5734487a14414
kmr <ettedlop@gmail.com>
2022-12-04 20:31:23 +0900

    DTO

---
+++ RegularDTO.java
@@ -0,0 +1,8 @@
+package classes;
+class RegularDTO 
+{
+	RegularDTO reg01[] = {new RegularDTO("R001","보라","2020-03-05",250),new RegularDTO("R002","뚜비","2019-03-03",300)};
+	public static void main(String[] args) 
+	{
+	}
+}
